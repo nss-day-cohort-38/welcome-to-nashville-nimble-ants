03f2f34fb8a6be950736eaa9a8126459230f22e6
Matt Crook <matt.crook11@gmail.com>
2020-01-29 10:23:10 -0600

    wip commit

--- src/scripts/restaurants/restaurants-toHTML.js
+++ src/scripts/restaurants/restaurants-toHTML.js
@@ -1,42 +1,19 @@
+
 let num = 0;
 
-const createRestaurantComponent = ({ name, address }) => {
-  num++;
-  return `
-      <div id="result-blocks-${num}">
-      <section id="restaurant-name">${name}</section>
-      <section>${address}</section>
-      <button id="rest__saveBtn_${num}">Save</button></<button>
-      </div>
-      `;
-};
-
-const renderRestaurant = restaurantHTML => {
-  const searchResultsContainer = document.getElementById("search-results");
-  searchResultsContainer.innerHTML = "";
-  searchResultsContainer.innerHTML += restaurantHTML;
-};
-/*
-let num = 0;
-
-const createRestaurantComponent = {
-  restaurantFactory= ({ name, address }) {
-    const address = JSON.parse(park.mapped_location.human_address);
+  const restaurantFactory = ({ name, address }) => {
     num++;
-    rreturn `
-      <div class="result-blocks-${num}">
+    return `
+      <section id="result-blocks-${num}">
       <section id="restaurant-name">${name}</section>
       <section>${address}</section>
       <button id="rest__saveBtn_${num}">Save</button></<button>
       </div>
       `;
-  },
-  renderSearchResults(searchResults) {
+  };
+
+   const renderSearchResults = (restaurantHTML) => {
     const container = document.getElementById("search-results");
     container.innerHTML = "";
-    searchResults.forEach(park => {
-      container.innerHTML += this.parksFactory(park);
-    });
-  }
-};
-*/
+      container.innerHTML += restaurantHTML
+    };
