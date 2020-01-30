460a4de838e3a0a1a304d38a3ccb67d8e8f3737c
Matt Crook <matt.crook11@gmail.com>
2020-01-30 13:20:26 -0600

    Restaurants search by cuisine (#14)
    * feat:parse API, main functionality of search event handling implemented.
    
    * fix: dropdown add. Search by cuisine instead of name. Dynammically populate dropdwon from API query. Fix save button to grab correct event click.
    
    * WIP commit. renderComponents undefined
    
    * fix: restaurants functionality fixed and load tag.
    
    * Parks save button working after restaurants
    
    * fix: API key fix.
    
    * fix:white space in apimanger file.
    
    * fix: delete console.log
    
    * fix: loader
    
    * fix: loader fix 2

--- src/scripts/restaurants/restaurantsAPIManager.js
+++ src/scripts/restaurants/restaurantsAPIManager.js
@@ -1,28 +1,59 @@
+const cuisinesURL = `https://developers.zomato.com/api/v2.1/cuisines?city_id=1138&apikey=${restaurantKey}`;
+fetch(cuisinesURL)
+  .then(response => response.json())
+  .then(cuisines => {
+    let select = document.createElement("select");
+    select.id = "cuisineSelect";
+    let options = "";
+    cuisines.cuisines.forEach(({ cuisine }) => {
+      options +=
+        '<option value="' +
+        cuisine.cuisine_id +
+        '">' +
+        cuisine.cuisine_name +
+        "</option>";
+    });
+    select.innerHTML = options;
+    const restaurantSearch = document.getElementById("search-restaurants");
+    const restaurantSearchBtn = document.getElementById(
+      "search-restaurants-btn"
+    );
+    restaurantSearch.insertBefore(select, restaurantSearchBtn);
+  });
+
 /* let loader = `<div class="boxLoading">Loading Searches...</div>`;
 document.getElementById("search-results").innerHTML = loader; */
 
 document
   .getElementById("search-restaurants-btn")
   .addEventListener("click", () => {
-    const URL = `https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&start=first&sort=rating&apikey=c460f1e9d15428b644335618a7e14819`;
+    const cuisine = document.getElementById("cuisineSelect").value;
+    const URL = `https://developers.zomato.com/api/v2.1/search?entity_id=1138&cuisines=${cuisine}&entity_type=city&apikey=${restaurantKey}`;
+    let loader = `<div class="boxLoading">Loading Searches...</div>`;
+    document.getElementById("search-results").innerHTML = "";
     fetch(URL)
       .then(response => response.json())
       .then(restaurants => {
         const restaurantList = restaurants.restaurants;
-        // console.log(restaurantList);
+        //console.log(restaurantList);  // drilled down inside object to get the array of restaurant objects.
         const restaurantData = restaurantList.map(({ restaurant }) => {
+          // iterating over restaurantList, returning object of data we want.
           return {
             name: restaurant.name,
+            cuisines: restaurant.cuisines,
             address: restaurant.location.address
           };
         });
         renderComponents(restaurantData);
-        const restaurantSearchInput = document.getElementById("search-restaurants");
-        restaurantSearchInput.value = "";  // clears search box after we render the results aka the restaurant component
-        //console.log(restaurantData); // the array drilled down to name and address
+
+        const restaurantSearchInput = document.getElementById(
+          "search-restaurants"
+        );
+        restaurantSearchInput.value = "";
+        // clears search box after we render the results aka the restaurant component
       });
-    //.catch(err => {
-    // instead of calling renderComponents write a message something like
-    // const searchResultsContainer = document.getElementById("search-results");
-    // searchResultsContainer.innerHTML = "No Restaurants Found";
   });
+//.catch(err => {
+// instead of calling renderComponents write a message something like
+// const searchResultsContainer = document.getElementById("search-results");
+// searchResultsContainer.innerHTML = "No Restaurants Found";
